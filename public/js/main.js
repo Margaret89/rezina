d567a4d7d92fdb2133ef8e74323c14939df0a97c
katyush14 <katyushina89@gmail.com>
2020-02-21 12:47:24 +0300

    header bottom

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,29 @@
 $(document).ready(function () {
 // Табуляция
-	if ($('.js-tabs-page').length) {
-		if (!$('.js-tabs-page-item').hasClass('active')) {
-			$('.js-tabs-page-list').each(function(){
-				$(this).find('.js-tabs-page-item:first').addClass("active");
+	tabPage('.js-tabs-page');
+	tabPage('.js-tabs-inner');
+
+	function tabPage(name) {
+		if ($(name).length) {
+			$(name+'-list').each(function(){
+				$(this).find(name+'-item:first').addClass("active");
 			});
 
-			$('.js-tabs-page-content').each(function(){
-				$(this).find('.js-tabs-page-content-item:first').addClass("active");
-				$(this).find('.js-tabs-page-content-item:first').fadeIn();
+			$(name+'-content').each(function(){
+				$(this).find(name+'-content-item:first').fadeIn();
 			});
-		}else{
-			$('.js-tabs-page-content').find('#' + $('.js-tabs-page-item.active').attr('data-item')).addClass("active");
-			$('.js-tabs-page-content').find('#' + $('.js-tabs-page-item.active').attr('data-item')).fadeIn();
+
+			$(name+'-item').click(function(e) {
+				e.preventDefault();
+				var $parent = $(this).parents(name);
+
+				$parent.find(name+'-content-item').hide();
+				$parent.find(name+'-item').removeClass('active');
+
+				$(this).addClass("active");
+				$parent.find('#' + $(this).attr('data-item')).fadeIn();
+			});
 		}
-
-		$('.js-tabs-page-item').click(function(e) {
-			e.preventDefault();
-			var $parent = $(this).parents('.js-tabs-page');
-
-			$parent.find('.js-tabs-page-content-item').hide();
-			$parent.find('.js-tabs-page-content-item').removeClass('active');
-			$parent.find('.js-tabs-page-item').removeClass('active');
-
-			$(this).addClass("active");
-			$parent.find('#' + $(this).attr('data-item')).addClass("active");
-			$parent.find('#' + $(this).attr('data-item')).fadeIn();
-		});
 	}
 
 // Стилизация выпадающего списка
